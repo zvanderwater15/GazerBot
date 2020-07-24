ce7468a6112e3f9cc4619aeb401b11cded1e3fbc
zvanderwater15 <zvanderwater15@georgefox.edu>
2020-07-24 00:39:09 -0700

    initialize python project file structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gazerbot',
+      version='1.0',
+      description='Shoegaze Lyric Generator',
+      author='Zoe vanderWater',
+      author_email='zoe.van42@gmail.com',
+      url='https://github.com/zvanderwater15/GazerBot',
+      packages=['gazerbot']
+     )
