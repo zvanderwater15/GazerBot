92cd7caf367d5de21b5e88e4432ad11e5991c567
zvanderwater15 <zvanderwater15@georgefox.edu>
2020-09-13 17:48:43 -0700

    calculate probability of a line's length given a chunk of text

--- gazerbot/analyzers.py
+++ gazerbot/analyzers.py
@@ -1,3 +1,6 @@
+import numpy as np
+from collections import Counter
+
 def average_word_count(lyrics_list):
     def word_count(lyrics):
         return len(lyrics.split())
@@ -16,3 +19,11 @@ def analyze_structure(lyrics):
     return {"verses": [], "choruses":[]}
 
 
+def line_length_probability(lyrics):
+    lines = lyrics.split('\n')
+    lengths = [len(line.split(" ")) for line in lines if len(line.split(" ")) > 1]
+    freq = Counter(lengths)
+    lengths_set = list(freq.keys())
+    weights = np.array(list(freq.values()),dtype=np.float64)
+    weights /= weights.sum()   # Normalize weights to sum to 1.
+    return lengths_set, weights
