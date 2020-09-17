26af0ec6587d1d3592fb3889109ea310e7345d56
zvanderwater15 <zvanderwater15@georgefox.edu>
2020-09-16 23:26:31 -0700

    check validity of lyrics from genius, save lyrics to files for later

--- gazerbot/genius.py
+++ gazerbot/genius.py
@@ -1,7 +1,17 @@
 import lyricsgenius
-from gazerbot import secrets
+import re
+from gazerbot import secrets, helpers
 
-def get_lyrics(artist, song):
+def get_lyrics(artist, title):
     genius = lyricsgenius.Genius(secrets.GENIUS_TOKEN)
-    song = genius.search_song(song, artist)
-    return song.lyrics if song else None
+    song = genius.search_song(title, artist=artist)
+
+    if (song and helpers.normalize(song.title) not in helpers.normalize(title)):
+        print(f"MISMATCH ERROR: {song.title}")
+        return None
+    elif not song or len(song.lyrics) <= 1:
+        print(f"NO LYRICS: {title}")
+        return None
+    else:
+        song.save_lyrics(f"lyrics/db/{helpers.normalize(artist+title)}", sanitize=False, overwrite=True)
+        return song.lyrics
