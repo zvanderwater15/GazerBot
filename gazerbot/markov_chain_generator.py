c923c756f79564a94d26956cc4dbcb031e12a901
zvanderwater15 <zvanderwater15@georgefox.edu>
2020-10-11 23:03:27 -0700

    create song generator only once

--- gazerbot/markov_chain_generator.py
+++ gazerbot/markov_chain_generator.py
@@ -4,13 +4,23 @@ import re
 from collections import defaultdict
 from gazerbot import helpers, analyzers
 
-def markov_chain_generator(lyrics_list, num_words):
-    song = ""
+def generate_songs(lyrics_list, song_length, num_to_generate):
+  songs = []
+  markov_graph = create_graph(lyrics_list)
+  for i in range(num_to_generate):
+    songs.append(markov_chain_generator(markov_graph, song_length))
+  return songs
+
+def markov_chain_generator(markov_graph, num_words):
+    song = ' '
+    song = ' '.join(_walk_graph(markov_graph, distance=(num_words)))
+    return song
+
+def create_graph(lyrics_list):
     all_lyrics = helpers.join_and_filter_all_lyrics(lyrics_list)
     word_list = helpers.lyrics_to_word_list(all_lyrics)
-    word_list = helpers.only_english_words(word_list)
+
     # Create graph.
-    #TODO create separate graph for each song then combine them
     markov_graph = defaultdict(lambda: defaultdict(int))
 
     last_word = word_list[0].lower()
@@ -18,10 +28,8 @@ def markov_chain_generator(lyrics_list, num_words):
         word = word.lower()
         markov_graph[last_word][word] += 1
         last_word = word
-
-    song = ' '.join(_walk_graph(markov_graph, distance=(num_words+40)))
-
-    return song
+    
+    return markov_graph
 
 def _walk_graph(graph, distance=5, start_node=None):
   """Returns a list of words from a randomly weighted walk."""
@@ -31,7 +39,6 @@ def _walk_graph(graph, distance=5, start_node=None):
   # If not given, pick a start node at random.
   if not start_node:
     start_node = random.choice(list(graph.keys()))
-  
   
   weights = np.array(
       list(graph[start_node].values()),
--- scripts/run.py
+++ scripts/run.py
@@ -1,12 +1,12 @@
 from gazerbot import spotify, genius, markov_chain_generator, naive_shuffler, analyzers, secrets, helpers
 import re
+import random 
 
 lyrics_list = []
-results_file = "dead_or_dreaming.txt"
-rand_lyrics_to_generate = 3
-markov_lyrics_to_generate = 20
+results_file = "personalmegaplaylist.txt"
+markov_lyrics_to_generate = 40
 user = 'zoiaran'
-playlist = 'am i dead or am i dreaming'
+playlist = 'All Monthly Playlists 2017-?'
 helpers.write_to_file(results_file, playlist)
 for track in spotify.get_tracks_from_playlist(user, playlist):
     lyrics = genius.get_lyrics(track["artist"], track["title"])
@@ -18,19 +18,12 @@ average_line_count = analyzers.average_num_lines(lyrics_list)
 stats = f"STATS:\n AVE WORDS: {average_word_count}\nAVE LINES: {average_line_count}"
 helpers.write_to_file(results_file, stats)
 
-helpers.write_to_file(results_file, "FULLY RANDOMIZED LYRICS")
-for i in range(1, rand_lyrics_to_generate + 1):
-    generated_lyrics = naive_shuffler.shuffle_lyrics_naive(lyrics_list, average_line_count, average_word_count)
-    print(f"\n\n-----SONG {i}------")
-    helpers.write_to_file(results_file, f"\n\n-----SONG {i}------")
-    print(generated_lyrics)
-    helpers.write_to_file(results_file, generated_lyrics)
-
 helpers.write_to_file(results_file, "MARKOV CHAIN (basically predictive text) GENERATED LYRICS")
-for i in range(1, markov_lyrics_to_generate + 1):
-    generated_lyrics = markov_chain_generator.markov_chain_generator(lyrics_list, average_word_count)
-    print(f"\n\n-----SONG {i}------")
-    helpers.write_to_file(results_file, f"\n\n-----SONG {i}------")
-    print(generated_lyrics)
-    helpers.write_to_file(results_file, generated_lyrics)
+markov_songs = markov_chain_generator.generate_songs(lyrics_list, average_word_count + 40, markov_lyrics_to_generate)
+for song in markov_songs:
+    title = random.choice(song.split()) + random.choice(song.split())
+    print(f"\n\n----{title}------")
+    helpers.write_to_file(results_file, f"\n\n-----{title}------")
+    print(song)
+    helpers.write_to_file(results_file, song)
 
