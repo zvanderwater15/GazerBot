f8befd0928ba03f4ccbf652833aee7db5e97c117
zvanderwater15 <zvanderwater15@georgefox.edu>
2022-08-28 20:56:12 -0700

    add more tests

--- tests/test_genius.py
+++ tests/test_genius.py
@@ -7,4 +7,12 @@ def test_get_lyrics():
     artist = "The Beatles"
     song = "Yesterday"
     lyrics = genius.get_lyrics(artist, song)
+
     assert "All my troubles seemed" in lyrics
+
+def test_get_nonexistent_lyrics():
+    artist = "The Beatles"
+    song = "Not Yesterday"
+    lyrics = genius.get_lyrics(artist, song)
+
+    assert not lyrics
--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -2,8 +2,25 @@ from .context import gazerbot
 from gazerbot import spotify
 import pytest
 
+user = "zoiaran"
+invalid_user = "soiethsiuertaeuthyaeiutg"
+playlist = "ipod sim"
+invalid_playlist = "erseaeinkoiznhdtrr"
+
 def test_get_playlists():
-    user = "zoiaran"
     playlists = spotify.get_user_playlists(user)
     assert playlists
-    print(playlists)
+    assert playlists[0]["name"]
+    assert playlists[0]["id"]
+
+def test_get_playlists_for_nonexistent_user():
+    playlists = spotify.get_user_playlists(invalid_user)
+    assert not playlists
+
+def test_get_tracks_from_playlist():
+    tracks = spotify.get_tracks_from_playlist(user, playlist)
+    assert tracks
+
+def test_get_tracks_from_nonexistent_playlist():
+    tracks = spotify.get_tracks_from_playlist(user, invalid_playlist)
+    assert not tracks
